b01a5fed0801a422cced139c2a6e304c9e1f9525
JainilModi <jainilrmodi2013@gmail.com>
2023-11-21 21:49:58 +0530

    add cors

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,17 @@ import { PORT, mongodbURL } from './config.js';
 import mongoose from 'mongoose';
 import { Book } from './models/bookModel.js';
 import router from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());   //Middleware for parsing request body
 app.use('/', router);
+//app.use(cors());   //Middleware for handling CORS policy (allows all origins)
+app.use(cors({
+    origin:'http://localhost:5000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}))
 
 
 app.listen(PORT, () => {
